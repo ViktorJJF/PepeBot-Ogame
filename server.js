b2573cea56e8fe43982915f3cfb84ad2cfcd5966
Victor Juan Jimenez Flores <vj.jimenez96@gmail.com>
2020-01-18 05:27:16 -0500

    deleted puppetear (temporal)

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const moment = require("moment");
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 // app.use(express.static(__dirname + "/public"));
 //loading bot
-require("./index.js");
+// require("./index.js");
+
+setInterval(() => {
+  console.log(
+    "refrescando ogame a las : ",
+    moment().format("MMMM Do YYYY, h:mm:ss a")
+  );
+}, 10000);
 
 app.get("/", (req, res) => {
   res.json({ bot: "Pepe bot", description: "boteando ando" });
